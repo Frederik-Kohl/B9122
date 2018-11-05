19b1f8f6bbbd472e5015af381b42f26c53a02f44
Frederik Kohl <frederikkohl@Frederiks-MBP.fios-router.home>
2018-11-05 02:21:46 -0500

    Second commit: added print statement

--- B9122_HW_1_Question_4.py
+++ B9122_HW_1_Question_4.py
@@ -11,3 +11,5 @@ for word in words:  # Iterate through list and count occurrences, adding each oc
 sorted_list = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)  # Sort list in decreasing order
 for i in range(0, 5):
     print((sorted_list[i])[1], (sorted_list[i])[0])  # Print first 5 words in list, but each tuple reversed
+
+print("I have changed this")
